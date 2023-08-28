833db194d431ebfdaffe1f45ee36dc05275cb9a9
Justin Clark <jclarksummit@gmail.com>
2023-08-27 21:54:12 -0400

    Add basic boilerplate

--- src/app.ts
+++ src/app.ts
@@ -12,3 +12,19 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 export default app;
+
+// /** Handle 404 errors -- this matches everything */
+// app.use(function (req, res, next) {
+//   throw new NotFoundError();
+// });
+
+// /** Generic error handler; anything unhandled goes here. */
+// app.use(function (err, req, res, next) {
+//   if (process.env.NODE_ENV !== "test") console.error(err.stack);
+//   const status = err.status || 500;
+//   const message = err.message;
+
+//   return res.status(status).json({
+//     error: { message, status },
+//   });
+// });
