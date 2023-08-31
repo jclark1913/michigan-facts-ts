b6a03293398bfdc7dc8d604f8628aa67a6320ec0
Justin Clark <jclarksummit@gmail.com>
2023-08-31 14:45:02 -0400

    Add basic error handling classes

---
+++ src/expressError.ts
@@ -0,0 +1,56 @@
+/** ExpressError extends normal JS error so we can
+ *  add a status when we make an instance of it.
+ *
+ *  The error-handling middleware will return this.
+ */
+
+class ExpressError extends Error {
+  status: number;
+  message: string;
+
+  constructor(message: string, status: number) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
+
+/** 404 NOT FOUND error. */
+
+class NotFoundError extends ExpressError {
+  constructor(message: string = "Not Found") {
+    super(message, 404);
+  }
+}
+
+/** 401 UNAUTHORIZED error. */
+
+class UnauthorizedError extends ExpressError {
+  constructor(message: string = "Unauthorized") {
+    super(message, 401);
+  }
+}
+
+/** 400 BAD REQUEST error. */
+
+class BadRequestError extends ExpressError {
+  constructor(message:string  = "Bad Request") {
+    super(message, 400);
+  }
+}
+
+/** 403 BAD REQUEST error. */
+
+class ForbiddenError extends ExpressError {
+  constructor(message: string = "Forbidden") {
+    super(message, 403);
+  }
+}
+
+export {
+  ExpressError,
+  NotFoundError,
+  UnauthorizedError,
+  BadRequestError,
+  ForbiddenError,
+};
