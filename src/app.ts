4f5c2b8d459a1e2a77fb80fc8ae3ac480f0448e9
Justin Clark <jclarksummit@gmail.com>
2023-08-31 10:05:33 -0400

    Add helmet to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 "use strict";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import morgan from "morgan";
 
 // const { NotFoundError } = require("./expressError");
